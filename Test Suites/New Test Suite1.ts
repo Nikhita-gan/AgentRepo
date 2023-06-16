e5473246c0831568be8bc1c8c44526d1be04eed7
nikhita.ganvir <nikhita.ganvir@INBL01LAP30757.brd.g-c-i.com>
2023-06-16 13:11:08 +0530

    3rd commit

--- Test Suites/New Test Suite1.ts
+++ Test Suites/New Test Suite1.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>4cbd6173-b7de-4d2d-8e31-73cacf3a44e2</testSuiteGuid>
+   <testCaseLink>
+      <guid>3cdc6bd5-e246-4362-b649-c0334ac052ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
